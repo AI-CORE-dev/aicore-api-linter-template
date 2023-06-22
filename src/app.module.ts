ca6f1fab3a44ec93509a7364cf3763ee1c34fa6d
Lionel Irungaray <104365127+Irungaray-AiC@users.noreply.github.com>
2023-06-22 20:33:18 -0300

    Fixes on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 
-import { AuthModule, CoreModule, SpaceModule } from '@worknmates/core/modules';
-import { LoggerMiddleware } from '@worknmates/core/utils/middlewares';
+import { FoundationalModule } from '@aicore/modules';
+import { LoggerMiddleware } from '@aicore/utils/middlewares';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [CoreModule, AuthModule, SpaceModule, SearchModule],
+  imports: [FoundationalModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).exclude('user/(.*)/image', 'company/(.*)/image', 'host/(.*)/image', 'host/(.*)/space/(.*)/image').forRoutes('*');
+  configure(consumer: MiddlewareConsumer): void {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude('(.*)/(.*)/image')
+      .forRoutes('*');
   }
 }
